8077a8038f7c96c414b80926981a0e3390d864dc
alex2000d <demarcoalessandro777@gmail.com>
2024-07-02 16:44:00 +0200

    [UPD] I cycle the arrey

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // creo un arrey
 let ingredienti = ['pomodoro', 'uova', 'pane', 'pasta', 'formaggio', 'tonno', 'affettati', 'biscotti', 'latte'];
 console.log(ingredienti)
-
+// recupero l'elemento che deve contenere la mia lista della spesa
+const lista_spesa = document.getElementById('lista-spesa');
+// ciclo l'arrey
+let i = 0
+ while ( i < ingredienti.length ){
+    
+    i++
+   
+}
