65aace5b8984b22b23f14a55b1a2373fc21317a0
alex2000d <demarcoalessandro777@gmail.com>
2024-07-02 16:44:33 +0200

    [UPD] exercise complete

--- js/script.js
+++ js/script.js
@@ -8,5 +8,10 @@ let i = 0
  while ( i < ingredienti.length ){
     
     i++
-   
+    // creo un nuovo elemento nella lista
+    const li = document.createElement('li');
+    // gli metto il testo all'interno
+    li.innerText = ingredienti[i];
+// appendo la lista all'interno di lista spesa
+    lista_spesa.append(li);
 }
